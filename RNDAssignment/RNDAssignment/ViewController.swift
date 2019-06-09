be963e8f6372833192437e4b454b2ad7ad4552b2
DeniSmilevska <deni.smilevska@gmail.com>
2019-06-09 17:39:18 +0200

    Initial commit (created project)

---
+++ RNDAssignment/RNDAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDAssignment
+//
+//  Created by Deni Smilevska on 6/9/19.
+//  Copyright © 2019 Deni Smilevska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
