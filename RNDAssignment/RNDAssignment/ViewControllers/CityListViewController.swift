02c5ac535fdecb72fd87643b030252d91467ce05
DeniSmilevska <deni.smilevska@gmail.com>
2019-06-09 18:43:44 +0200

    filtering

--- RNDAssignment/RNDAssignment/ViewControllers/CityListViewController.swift
+++ RNDAssignment/RNDAssignment/ViewControllers/CityListViewController.swift
@@ -12,10 +12,11 @@ protocol CitySelectedDelegate{
     func citySelected(city: City)
 }
 
-class CityListViewController: UIViewController {
+class CityListViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     var cityList: [City] = []
+    var filteredList: [City] = []
     var delegate: CitySelectedDelegate?
     
     override func viewDidLoad() {
@@ -31,6 +32,7 @@ class CityListViewController: UIViewController {
                 cityList.sort { (c1, c2) -> Bool in
                     return c1 < c2
                 }
+                filteredList = cityList
                 self.tableView.reloadData()
             }
             catch let error{
@@ -39,16 +41,31 @@ class CityListViewController: UIViewController {
         }
         // Do any additional setup after loading the view.
     }
-    
+    //MARK: - Search delegate
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        refreshFilteredList(prefix: searchText)
+        self.tableView.reloadData()
+    }
+    //MARK: - Helper functions
+    func refreshFilteredList(prefix: String){
+        self.filteredList.removeAll()
+        //have a filter list so we can work with that one instead of loading the whole cityList json file again.
+        for city in self.cityList{ // cities are sorted so we dont have to sort them again after adding them to the filter list
+            if city.description.starts(with: prefix) {
+                self.filteredList.append(city)
+            }
+        }
+    }
 }
+
 extension CityListViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.cityList.count
+        return self.filteredList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "CityTableViewCell", for: indexPath) as? CityTableViewCell{
-            let city = self.cityList[indexPath.row]
+            let city = self.filteredList[indexPath.row]
             cell.setupCellWith(city: city)
             return cell
         }
@@ -60,7 +77,7 @@ extension CityListViewController: UITableViewDelegate, UITableViewDataSource{
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         DispatchQueue.main.async {
-            let city = self.cityList[indexPath.row]
+            let city = self.filteredList[indexPath.row]
             self.delegate?.citySelected(city: city)
             if let mapsController = self.delegate as? MapsViewController{
                 self.splitViewController?.showDetailViewController(mapsController, sender: nil)
