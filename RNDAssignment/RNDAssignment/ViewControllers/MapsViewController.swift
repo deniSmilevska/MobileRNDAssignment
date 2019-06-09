985bf183ca5ffcef844910c817bc315cc011cdee
DeniSmilevska <deni.smilevska@gmail.com>
2019-06-09 18:27:44 +0200

    Maps controller added.

---
+++ RNDAssignment/RNDAssignment/ViewControllers/MapsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapsViewController.swift
+//  MobileRNDAssignment
+//
+//  Created by Deni Smilevska on 6/8/19.
+//  Copyright © 2019 Deni Smilevska. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapsViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
